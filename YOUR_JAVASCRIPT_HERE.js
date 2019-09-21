94781d472c19a012357b7228135d7a63537a6b3d
Remzi <ict@remzi.info>
2019-09-21 17:09:54 +0200

    Started coding

---
+++ YOUR_JAVASCRIPT_HERE.js
@@ -0,0 +1,37 @@
+// Write your JS here
+
+let hero = {
+    name: 'Remzi',
+    heroic: true,
+    inventory: [],
+    health: 10,
+    weapon: {
+        type: 'Sword',
+        damage: 2
+    },
+    resting: function() {
+        return rest;
+    }
+};
+
+
+
+
+
+// And (re)assigns the health property of that object the value 10
+function rest(params) {
+
+}
+
+console.log(rest());
+
+
+
+
+function pickUpItem(params) {
+    
+}
+
+function equipWeapon(params) {
+    
+}
